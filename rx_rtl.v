ce4345590e5464602cf16a82c87fc2ba4d25dfe1
Bishoy Yousry <1900733@eng.asu.edu.eg>
2022-12-21 03:40:04 +0200

    Implement tb for FSM_RX not finished, tb for DETECT_START_BIT, tb for SIPO

--- rx_rtl.v
+++ rx_rtl.v
@@ -1,23 +1,32 @@
+`timescale 1us/1us
+
 module RX(rst_n, clk, rx, rx_out, parity_err);
 
 input rst_n, clk, rx;
 output [10:0]rx_out;
 output parity_err;
+reg store;
 wire sipo_en, detect_start, stop_check, parity_en;
 wire[7:0] sipo_data_out, parity_out;
 
-SIPO sipo(.sipo_en(sipo_en), .rx(rx), .clk(clk), .rst_n(rst_n), .sipo_data_out(sipo_data_out));
-FSM_RX fsm_rx(.sipo_en(sipo_en), .rx(rx), .clk(clk), .rst_n(rst_n), .detect_start(detect_start), .stop_en(stop_check));
+SIPO_8BIT sipo_8bit(.sipo_en(sipo_en), .rx(rx), .clk(clk), .rst_n(rst_n), .sipo_data_out(sipo_data_out));
+FSM_RX fsm_rx(.sipo_en(sipo_en), .rx(rx), .clk(clk), .rst_n(rst_n));
 DETECT_START_BIT detect_start_bit(.clk(clk), .rx(rx), .detect_start(detect_start));
 PARITY_CHECK parity_check(.parity_err(parity_err), .parity_en(parity_en), .rx(rx), .rx_data_in(sipo_data_out), .parity_data_out(parity_data_out));
 
 endmodule
 
 
-module SIPO(sipo_en, rx, clk, rst_n, sipo_data_out);
+
+
+
+
+
+
+module SIPO_8BIT(sipo_en, rx, clk, rst_n, sipo_data_out);
   input sipo_en, rx, clk, rst_n;
-  output [10:0] sipo_data_out;
-  reg [10:0]shift_reg;
+  output [7:0] sipo_data_out;
+  reg [7:0]shift_reg;
   
   always@(posedge clk or negedge rst_n)
   begin
@@ -26,7 +35,7 @@ module SIPO(sipo_en, rx, clk, rst_n, sipo_data_out);
     else if(sipo_en)
       begin
       shift_reg <= (shift_reg >> 1);
-      shift_reg[10] <= rx;
+      shift_reg[7] <= rx;
       end
     else
       shift_reg <= shift_reg;
@@ -35,17 +44,46 @@ module SIPO(sipo_en, rx, clk, rst_n, sipo_data_out);
 endmodule
 
 
+/*Test bench of SIPO (Serial in parallel out shift register)*/
+module TB_SIPO_8BIT();
+  reg clk, rst_n, sipo_en, rx;
+  SIPO_8BIT sipo_8bit(.sipo_en(sipo_en), .rx(rx), .clk(clk), .rst_n(rst_n));
+  
+  always #1 clk = ~clk;
+  
+  initial begin
+    clk = 0; sipo_en = 0; rx = 1; rst_n = 0;
+    #3 rst_n = 1;
+    #2 rx = 0;
+    #2 sipo_en = 1; rx = 1; 
+    #2 rx = 0;
+    #2 rx = 1;
+    #2 rx = 0;
+    #2 rx = 1;
+    #2 rx = 0;
+    #2 rx = 1; 
+    #2 sipo_en = 0;
+  end
+endmodule
 
-module FSM_RX(sipo_en, rx, clk, rst_n, detect_start, stop_en);
+
+
+
+
+
+
+
+module FSM_RX(sipo_en, rx, clk, rst_n, detect_start);
   
-  input rx, clk, rst_n;
-  output reg sipo_en, detect_start, stop_en;
+  input rx, clk, rst_n, detect_start;
+  output reg sipo_en;
+  reg store;
   
-  parameter IDLE     =    3'd0,
-            COUNT_16 =    3'd1,
-            SHIFT    =    3'd2,
-            READ_BUFF=    3'd3,
-            STOP     =    3'd4;
+  parameter IDLE           =    3'd0,
+            COUNT_16       =    3'd1,
+            SHIFT          =    3'd2,
+            STORE_IN_FIFO  =    3'd3,
+            RX_DONE        =    3'd4;
             
   wire rx_start;
   reg [3:0] bit_count, count_16;
@@ -53,40 +91,47 @@ module FSM_RX(sipo_en, rx, clk, rst_n, detect_start, stop_en);
   always@(posedge clk or negedge rst_n) begin
   if(!rst_n) begin
     state <= IDLE;
-    sipo_en <= 1'b0;
-    detect_start <= 1'b0;
-    stop_en <= 1'b0;
+    store <= 0;
+    sipo_en <= 0;
   end
   else begin
     case(state)
       IDLE: 
-        if(detect_start) begin
+        if(detect_start) 
           state <= COUNT_16;          
-        end
       COUNT_16:
         if(count_16 == 4'b1111) begin
             state <= SHIFT;
             count_16 <= 4'b0000;
-            detect_start <= 1'b0;
             sipo_en <= 1'b1;
         end
-        else
+        else begin
           count_16 <= count_16 + 4'd1;        
-          
+          sipo_en <= 1'b0;
+        end
       SHIFT:
         if(bit_count >= 4'd11) begin
-          state <= READ_BUFF;
-          stop_en <= 1'b1;
-          sipo_en <= 1'b1;
+          
+          state <= STORE_IN_FIFO;
+          sipo_en <= 1'b0;
           
         end
         else begin
           bit_count <= bit_count + 4'd1;
           state <= COUNT_16;
-          sipo_en <= 1'b0;
+          sipo_en <= 1'b1;
         end
     
-      //READ_BUFF:
+      STORE_IN_FIFO:        
+        if(store) begin
+         state <= RX_DONE;
+         store <= 0;
+       end
+     else
+       store <= 1;     //APB interface should take the data
+      
+      RX_DONE:
+        state <= IDLE;
         
     endcase 
   end 
@@ -94,13 +139,53 @@ end
 endmodule
 
 
+
+module TB_FSM_RX();
+  reg rx, clk, rst_n, store, detect_start;
+  
+  FSM_RX fsm_rx(.rx(rx), .clk(clk), .rst_n(rst_n), .detect_start(detect_start));
+  
+  always #1 clk = ~clk;
+  
+  initial begin
+    clk <= 0; rst_n <= 0; store <= 0; detect_start <= 0;
+    #3 rst_n = 1;
+    #2 rx <= 0; detect_start <= 1;
+    #18 rx = 1;
+    #16 rx = 0;
+    #16 rx = 1;
+    #16 rx = 0;
+    #16 rx = 1;
+    #16 rx = 0;
+    #16 rx = 1;
+    #16 rx = 0;
+    #16 rx = 1;    
+  end
+endmodule
+
+
+
+
 module DETECT_START_BIT(clk, rx, detect_start);
   input clk, rx;
   output reg detect_start;
   
   always@(posedge clk) begin
-  if(rx == 1'b0 && detect_start == 1'b0) detect_start <= 1'b1;
-  else detect_start <= 1'b0;
+  if(rx == 1'b0 && detect_start == 1'b0)
+   detect_start <= 1'b1;
+  else detect_start <= detect_start;
+  end
+endmodule
+
+
+module TB_DETECT_START_BIT();
+  reg clk, rx;
+  wire detect_start;
+  DETECT_START_BIT detect_start_bit(.clk(clk), .rx(rx), .detect_start(detect_start));
+  always #1 clk = ~clk;
+  initial begin
+    clk = 0; rx = 1;
+    #3 rx = 0;
   end
 endmodule
 
@@ -128,3 +213,4 @@ else
   parity_err = 0;
   end
 endmodule
+
